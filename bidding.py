beea960136600a8b114ea28550545d63abe33aaa
Galbanyno <guido.mancinelli@mail.polimi.it>
2024-05-16 17:54:31 +0200

    Multiplicative pacing agent

--- bidding.py
+++ bidding.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class Auction:
@@ -33,6 +34,52 @@ class SecondPriceAuction(Auction):
         second = adRanking[-2]
         payment = values[second] / self.clickThroughRates[winners]
         return payment.round(2)
+
+
+class MultiplicativePacingAgent:
+    def __init__(self, valuation, budget, nRounds):
+        self.valuation = valuation
+        self.budget = budget
+        self.eta = 1/np.sqrt(nRounds)
+        self.maxRounds = nRounds
+        self.rho = self.budget / self.maxRounds
+        self.lmbd = 1
+        self.t = 0
+        self.totWins = 0
+        self.bidHist = np.array([])
+        self.utilitityHist = np.array([])
+        self.budgetHist = np.array([])
+
+    def bid(self):
+        if self.budget < 1:
+            return 0
+        bid = self.valuation / (self.lmbd + 1)
+        self.bidHist = np.append(self.bidHist, bid)
+        return bid
+
+    def update(self, win, utility, cost):
+        self.lmbd = np.clip(self.lmbd - self.eta * (self.rho - cost),
+                            a_min=0, a_max=1 / self.rho)
+        self.budget -= cost
+        self.totWins += win
+        self.utilitityHist = np.append(self.utilitityHist, utility)
+        self.budgetHist = np.append(self.budgetHist, self.budget)
+
+    def returnHistory(self):
+        return self.totWins, self.bidHist, self.utilitityHist, self.budgetHist
+
+    def plotHistory(self):
+        plt.plot(np.cumsum(self.utilitityHist))
+        plt.xlabel('$t$')
+        plt.ylabel('$Cumulative utility$')
+        plt.title('Utility history')
+        plt.show()
+
+        plt.plot(self.budgetHist)
+        plt.xlabel('$t$')
+        plt.ylabel('$Budget$')
+        plt.title('Budget history')
+        plt.show()
 
 
 class VCGAuction(Auction):
@@ -74,18 +121,18 @@ class FirstPriceAuction(Auction):
         return payment.round(2)
 
 
-def getClairvoyantTruthful(budget, myValuation, maxBids_t, nUsers):
-    utility = (myValuation - maxBids_t) * (myValuation >= maxBids_t)
+def getDeterministicClairvoyant(budget, myValuation, maxBids, nRounds):
+    utility = (myValuation - maxBids) * (myValuation >= maxBids)
     sortedRoundUtility = np.flip(np.argsort(utility))
-    clairvoyantUtilities = np.zeros(nUsers)
-    clairvoyantBids = np.zeros(nUsers)
-    clairvoyantPayments = np.zeros(nUsers)
-    c = 0
-    i = 0
-    while c <= budget - 1 and i < nUsers:
-        clairvoyantBids[sortedRoundUtility[i]] = 1
-        clairvoyantUtilities[sortedRoundUtility[i]] = utility[sortedRoundUtility[i]]
-        clairvoyantPayments[sortedRoundUtility[i]] = maxBids_t[sortedRoundUtility[i]]
-        c += maxBids_t[sortedRoundUtility[i]]
-        i += 1
-    return clairvoyantBids, clairvoyantUtilities, clairvoyantPayments
+    clairvoyantUtilities = np.zeros(nRounds)
+    clairvoyantBidding = np.zeros(nRounds)
+    clairvoyantPayments = np.zeros(nRounds)
+    totPayments = 0
+    roundInd = 0
+    while totPayments <= budget - 1 and roundInd < nRounds:
+        clairvoyantBidding[sortedRoundUtility[roundInd]] = 1
+        clairvoyantUtilities[sortedRoundUtility[roundInd]] = utility[sortedRoundUtility[roundInd]]
+        clairvoyantPayments[sortedRoundUtility[roundInd]] = maxBids[sortedRoundUtility[roundInd]]
+        totPayments += clairvoyantPayments[sortedRoundUtility[roundInd]]
+        roundInd += 1
+    return clairvoyantBidding, clairvoyantUtilities, clairvoyantPayments
--- biddingMain.py
+++ biddingMain.py
@@ -1,4 +1,71 @@
+import matplotlib.pyplot as plt
+
 from bidding import *
 
 if __name__ == '__main__':
+    # Auction settings
+    nAdvertisers = 4
+    clickThroughRates = np.ones(nAdvertisers)
+    myValuation = 0.8
+    budget = 100
+    nRounds = 1000
+    minBid = 0.01
+    maxBid = 1
+
+    # Competitors
+    othersValuation = 0.6
+    # if I use a really low mean bid for the competitor, the MPAgent performs better than the clairvoyant for most of the time!
+    otherBids = np.clip(np.random.normal(othersValuation, 0.1, size=(nAdvertisers - 1, nRounds)), minBid, maxBid)
+    highestBids = otherBids.max(axis=0)
+
+    # Clairvoyant agent
+    clairvoyantBidding, clairvoyantUtilities, clairvoyantPayments = getDeterministicClairvoyant(budget, myValuation, highestBids, nRounds)
+
+    # Multiplicative pacing agent
+    mpAgent = MultiplicativePacingAgent(myValuation, budget, nRounds)
+
+    # Start the auction
+    auction = SecondPriceAuction(clickThroughRates)
+    for aucInd in range(nRounds):
+        myBid = mpAgent.bid()
+        bids_t = np.append(myBid, otherBids[:, aucInd].ravel())
+        winner_t, payment_t = auction.round(bids_t)
+        myWin = int(winner_t == 0)
+        myUtility = (myValuation - highestBids[aucInd]) * myWin
+        myPayment = highestBids[aucInd] * myWin
+        mpAgent.update(myWin, myUtility, myPayment)
+
+    myWins, myBidHist, myUtilityHist, myBudgetHist = mpAgent.returnHistory()
+
+    # Plots
+    plt.plot(highestBids, label="Other Bids")
+    plt.plot(myBidHist, label="MP Agent Bids")
+    # plt.plot(clairvoyantPayments+minBid, label="Clairvoyant bids")
+    plt.legend()
+    plt.xlabel('$t$')
+    plt.ylabel('$Bids$')
+    plt.title('Bids history')
+    plt.show()
+
+    plt.plot(np.cumsum(myUtilityHist), label='MP Agent utilities')
+    plt.plot(np.cumsum(clairvoyantUtilities), label='Clairvoyant utilities')
+    plt.legend()
+    plt.xlabel('$t$')
+    plt.ylabel('$Cumulative utility$')
+    plt.title('Utility history')
+    plt.show()
+
+    plt.plot(myBudgetHist, label='MP Agent Budget')
+    plt.plot(budget-np.cumsum(clairvoyantPayments), label='Clairvoyant Budget')
+    plt.legend()
+    plt.xlabel('$t$')
+    plt.ylabel('$Budget$')
+    plt.title('Budget history')
+    plt.show()
+
+    plt.plot(np.cumsum(clairvoyantUtilities-myUtilityHist), label='MP Agent Regret')
+    plt.xlabel('$t$')
+    plt.ylabel('Regret')
+    plt.title('Cumulative regret of MP Agent')
+    plt.show()
 
